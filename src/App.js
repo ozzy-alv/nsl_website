3a09704cd3f2da8c1e43d4aea143a3dca9ebe96c
Ozzy <ozzyalvarado96@gmail.com>
2022-11-29 20:04:05 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React from "react";
-import { Box } from "@mui/material";
-import videoBg from "./assets/purp.mp4";
-import { Routes, Route } from "react-router-dom";
-import { Hero, About, Gallery, Contact, Product } from "./container";
-import { Navbar } from "./components";
-import "./styles/App.css";
+import React from 'react';
+import videoBg from './assets/purp.mp4';
+import { Routes, Route } from 'react-router-dom';
+import { Hero, About, Gallery, Contact, Product } from './container';
+import { Navbar } from './components';
+import './styles/App.css';
 
 const App = () => {
   return (
-    <Box className='main'>
-      <Box className='overlay' />
-      <video src={videoBg} autoPlay loop muted className='video' />
+    <div className="main">
+      <div className="overlay" />
+      <video src={videoBg} autoPlay loop muted className="video" />
 
-      <Box className='main-content'>
-        <Box
-          className='navbar'
-          style={{ maxWidth: { sx: "auto", md: "1280px" } }}
+      <div className="main-content">
+        <div
+          className="navbar"
+          style={{ maxWidth: { sx: 'auto', md: '1280px' } }}
         >
           <Navbar />
-        </Box>
+        </div>
 
-        <Box className='main-hero'>
+        <div className="main-hero">
           <Routes>
-            <Route path='/' element={<Hero />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/product' element={<Product />} />
-            <Route path='/gallery' element={<Gallery />} />
-            <Route path='/contact' element={<Contact />} />
+            <Route path="/" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
-        </Box>
-      </Box>
-    </Box>
+        </div>
+      </div>
+    </div>
   );
 };
 
